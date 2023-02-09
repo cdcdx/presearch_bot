e807c57c15e16f025dfab960329dbb703821e3b4
cdcdx <cdcdx888@gmail.com>
2023-02-09 14:17:43 +0800

    fix cmd

--- cmd_4_login.bat
+++ cmd_4_login.bat
@@ -4,9 +4,9 @@ title 登录账号
 echo.
 
 if (%1%)==() (
-  email="cdcdx888@gmail.com"
+  set email="cdcdx888@gmail.com"
 ) else (
-  email=%1%
+  set email=%1%
 )
 
 "C:/Program Files/Python311/python.exe" %~dp0/4.login_email.py %email%
